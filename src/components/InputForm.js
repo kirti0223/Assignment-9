cc545883fe3a7aa1f449b390b43325aaa7993eef
kirti0223 <68126346+kirti0223@users.noreply.github.com>
2021-10-14 22:38:06 +0530

    Add files via upload

---
+++ src/components/InputForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import styles from "./InputForm.module.css";
+
+const InputForm = (props) => {
+  const [input, setInput] = useState("");
+
+  const inputChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    props.addTodo(input);
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler} className={styles["todo-input"]}>
+      <label htmlFor="todo">To-do List</label>
+      <input
+        type="text"
+        id="todo"
+        onChange={inputChangeHandler}
+        value={input}
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default InputForm;
